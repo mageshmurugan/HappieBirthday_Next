31c3e1773b148958790ec0f9f16f84d8bdf746b5
MageshMurugan <mageshmurugan64@gmail.com>
2024-06-01 18:20:59 +0530

    silly mistake seven

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,6 +1,9 @@
 export default async function cron(req, res) {
-  const loggedInResponse = await fetch("https://happiebirthday.onrender.com/", {
-    method: "POST",
-  });
+  const loggedInResponse = await fetch(
+    "https://happiebirthday.onrender.com/email",
+    {
+      method: "POST",
+    }
+  );
   res.status(200).json({ message: "Cron job ran successfully" });
 }
