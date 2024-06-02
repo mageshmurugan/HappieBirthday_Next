022f431c3352760c61a6b1c31b57e4f11dd8a897
MageshMurugan <mageshmurugan64@gmail.com>
2024-06-02 10:32:23 +0530

    edited favicon and metadata

--- pages/api/cron.js
+++
@@ -1,9 +0,0 @@
-export default async function cron(req, res) {
-  const loggedInResponse = await fetch(
-    "https://happiebirthday.onrender.com/email",
-    {
-      method: "POST",
-    }
-  );
-  res.status(200).json({ message: "Cron job ran successfully" });
-}
